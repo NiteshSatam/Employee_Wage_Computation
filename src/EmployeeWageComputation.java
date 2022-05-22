88683af5ff5e69c5b36be26c7257d3d9bfc2bc2d
NiteshSatam <niteshsatam55@gmail.com>
2022-05-22 18:38:16 +0530

    Added wage calculation logic

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,19 +1,30 @@
 import java.util.Random;
 
-public class Employee_Wage_Computation {
+public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation!!!!");
+		
+		int wagePerHour=20;
+		int fullDayHour=8;
+		int halfDayHour=4;
+		int wagePerDay=0;
 
 		Random random = new Random();
 		int attendance = random.nextInt(3);
 
-		if (attendance == 2)
+		if (attendance == 2){
 			System.out.println("Employee is present full time.");
-		else if (attendance == 1)
+			wagePerDay=wagePerHour*fullDayHour;
+		}
+		else if (attendance == 1){
 			System.out.println("Employee is present half time.");
+			wagePerDay=wagePerHour*halfDayHour;
+		}
 		else
 			System.out.println("Employee is absent.");
+		
+		System.out.println("Daily salary of employee is:"+wagePerDay);
 
 	}
 
