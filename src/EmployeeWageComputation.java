bf4c4c58108d14151b2f7569c84f2ec5dad2b820
NiteshSatam <niteshsatam55@gmail.com>
2022-05-23 23:36:02 +0530

    Modified 20 days & 100 hrs logic

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation!!!!");
+		System.out.println("Welcome to Employee Wage Computation!!!!!");
 
 		int wagePerHour = 20;
 		int fullDayHour = 8;
